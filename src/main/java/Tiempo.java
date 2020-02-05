cdaad29f4160aa28201c9734fff0978a6ce55edc
Xavier De Anta <xdeantabelisario@danielcastelao.org>
2020-02-05 14:51:16 +0100

    Tercer commit

--- src/main/java/Tiempo.java
+++ src/main/java/Tiempo.java
@@ -9,7 +9,7 @@ public class Tiempo {
    public static void main(String[] args) throws IOException {
        Document doc=Jsoup.connect("http://www.google.com").userAgent("Mozilla").get();
        Elements elemento = doc.getElementsByClass("today_nowcard-temp");
-       //elemento
-       System.out.println(elemento.text());
+       String texto=elemento.text();
+       System.out.println(texto);
     }
 }
